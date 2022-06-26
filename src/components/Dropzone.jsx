aade340b730e8d1e6c6a046c08b2a6eee67af484
Danil <dvagapov@lab-code.com>
2022-06-26 12:42:19 +0500

    Updated dropzone

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDropzone } from 'react-dropzone';
+import axiosRequest from "../helpers/api";
 
 const Dropzone = () => {
   const { acceptedFiles, fileRejections, getRootProps, getInputProps } = useDropzone({
@@ -10,12 +11,21 @@ const Dropzone = () => {
     maxFiles: 1
   });
 
-  useEffect(() => {console.log(acceptedFiles)}, [acceptedFiles]);
-  useEffect(() => {console.log(fileRejections)}, [fileRejections]);
+  useEffect(() => {
+    const formData = new FormData();
+    const [file] = acceptedFiles;
+    formData.append("file", file);
+
+    axiosRequest.post("/api/image", formData, {'name': 'q'})
+      .then((res) => {
+      })
+      .catch((error) => {
+      });;
+  }, [acceptedFiles]);
 
   return (
     <section className="dropzone-container">
-      <div {...getRootProps({className: 'dropzone'})}>
+      <div {...getRootProps({ className: 'dropzone' })}>
         <input {...getInputProps()} />
         <p>Переместите сюда файлы, или кликните, чтобы выбрать</p>
         <em>(Только *.jpeg и *.png поддерживаются)</em>
